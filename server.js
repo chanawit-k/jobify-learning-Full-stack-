a4b0e925c27705e610ad06d1158ff29113eba7bd
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-12 12:04:10 +0700

    - job controller + router

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
-import { value } from './test.js'
-console.log(value)
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import morgan from 'morgan'
+import jobRouter from './routers/jobRouter.js'
+
+const app = express()
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use('/api/v1/jobs', jobRouter)
+
+// Not Found Handle
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' })
+})
+
+// Error Handle
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({ msg: 'something went wrong' })
+})
+
+const port = process.env.PORT || 5100
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`)
+})
