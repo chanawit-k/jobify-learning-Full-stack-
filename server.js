33d1a5d6723cbccc988d9bb14df4acebfbeddb8e
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-12 15:42:42 +0700

    - connect DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import mongoose from 'mongoose'
 import morgan from 'morgan'
 import jobRouter from './routers/jobRouter.js'
 
@@ -25,6 +26,12 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 5100
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`)
-})
+try {
+  await mongoose.connect(process.env.MONGO_URI)
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
