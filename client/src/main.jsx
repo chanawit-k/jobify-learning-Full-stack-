b90bb4da3143a75debb94347951fb99a13002039
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-14 12:58:59 +0700

    - install toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
+import 'react-toastify/dist/ReactToastify.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <ToastContainer position="top-center" />
   </>
 )
