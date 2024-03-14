ffc242a140a3a85f83afdae6da42499c93f0d6aa
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-14 11:59:46 +0700

    = port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('*', (req, res) => {
 
 app.use(errorHandlerMiddleware)
 
-const port = 5100
+const port = process.env.PORT || 5100
 try {
   await mongoose.connect(process.env.MONGO_URI)
   app.listen(port, () => {
