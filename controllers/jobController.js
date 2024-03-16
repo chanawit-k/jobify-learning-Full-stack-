b80b421f8ef2e5428e9583bcc90f90cedb896e8b
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-16 08:37:19 +0700

    - api stat

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,7 @@
 import Job from '../models/JobModel.js'
 import { StatusCodes } from 'http-status-codes'
-import { NotFoundError } from '../errors/customErrors.js'
+import mongoose from 'mongoose'
+import day from 'dayjs'
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({})
@@ -31,4 +32,59 @@ export const updateJob = async (req, res) => {
 export const deleteJob = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(req.params.id)
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob })
+}
+
+export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$jobStatus', count: { $sum: 1 } } },
+  ])
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr
+    acc[title] = count
+    return acc
+  }, {})
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  }
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: { $year: '$createdAt' },
+          month: { $month: '$createdAt' },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ])
+
+  console.log(
+    `monthlyApplications  ====> ${JSON.stringify(monthlyApplications)} `
+  )
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item
+
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format('MMM YY')
+      return { date, count }
+    })
+    .reverse()
+
+  res.status(StatusCodes.OK).json({ stats, monthlyApplications })
 }
