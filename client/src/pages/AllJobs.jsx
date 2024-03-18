d70e470364ffd4124641b514d4823d24831d5dcc
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-18 11:06:12 +0700

    - search fontend

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,18 +3,27 @@ import { JobsContainer, SearchContainer } from '../components'
 import customFetch from '../utils/customFetch'
 import { useLoaderData } from 'react-router-dom'
 import { useContext, createContext } from 'react'
-
 export const loader = async ({ request }) => {
+  console.log('loader')
   try {
-    const { data } = await customFetch.get('/jobs')
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ])
+    console.log(params)
+    const { data } = await customFetch.get('/jobs', {
+      params,
+    })
+
     return {
       data,
+      searchValues: { ...params },
     }
   } catch (error) {
-    toast.error(error?.response?.data?.msg)
+    toast.error(error.response.data.msg)
     return error
   }
 }
+
 const AllJobsContext = createContext()
 const AllJobs = () => {
   const { data } = useLoaderData()
