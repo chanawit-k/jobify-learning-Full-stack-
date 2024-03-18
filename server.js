6044dce60c4a155f120d989d70da963138116759
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-18 15:32:16 +0700

    - update static

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.resolve(__dirname, './client/dist')))
+app.use(express.static(path.resolve(__dirname, './public')))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -37,7 +37,7 @@ app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
 })
 
 // Not Found Handle
