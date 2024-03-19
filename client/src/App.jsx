0e45612e244fa9b203f5020ea20934050f7d1ed5
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-19 10:01:09 +0700

    - setup react-query + error elm stat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
   HomeLayout,
   Landing,
@@ -24,12 +26,21 @@ import { action as deleteJobAction } from './pages/DeleteJob'
 import { loader as adminLoader } from './pages/Admin'
 import { action as profileAction } from './pages/Profile'
 import { loader as statLoader } from './pages/Stats'
+import { ErrorElement } from './components'
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
   document.body.classList.toggle('dark-theme', isDarkTheme)
   return isDarkTheme
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 
 const isDarkThemeEnabled = checkDefaultTheme()
 
@@ -63,7 +74,12 @@ const router = createBrowserRouter([
             element: <AddJob />,
             action: addJobAction,
           },
-          { path: 'stats', element: <Stats />, loader: statLoader },
+          {
+            path: 'stats',
+            element: <Stats />,
+            loader: statLoader,
+            errorElement: <ErrorElement />,
+          },
           {
             path: 'all-jobs',
             element: <AllJobs />,
@@ -94,6 +110,11 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 export default App
